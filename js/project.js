6e7da7e84c899e5b0e725cb5021eb196b1743e10
Roberto Frenna <roberto.frenna@gmail.com>
2014-06-19 22:28:27 +0200

    Fix markdown parsing in project posts
    Also solved a very bad styling issue.

--- js/project.js
+++ js/project.js
@@ -3,29 +3,25 @@ $(document).ready(function() {
 
     $("#stdfrm").on('submit',function(event) {
         event.preventDefault();
-         $("#pmessage").html(loading+'...');
-         var news = $("#sendnews");
-         if(news.length)
-         {
-             news = news.is(':checked') ? '1' : '0';
-         }
-         else
-         {
-             news = '0';
-         }
-
-         N.json.project.newPost({message: $("#frmtxt").val(), to: $(this).data('to'), news: news },function(data) {
-            if(data.status == 'ok') {
-                $("#showpostlist").click();
-                $("#frmtxt").val('');
-            }
-            
-            $("#pmessage").html(data.message);
-
-            setTimeout(function() {
-                        $("#pmessage").html('');
-                        },5000);
-        });
+        var to = $(this).data('to');
+        setTimeout (function() {
+            $("#pmessage").html(loading+'...');
+            var news = $("#sendnews");
+            if(news.length)
+                news = news.is(':checked') ? '1' : '0';
+            else
+                news = '0';
+            N.json.project.newPost({message: $("#frmtxt").val(), to: to, news: news },function(data) {
+                if(data.status == 'ok') {
+                    $("#showpostlist").click();
+                    $("#frmtxt").val('');
+                }   
+                $("#pmessage").html(data.message);
+                setTimeout (function() {
+                    $("#pmessage").html('');
+                },5000);
+            });
+        }, 0);
     });
 
     $("#follow").click(function() {
